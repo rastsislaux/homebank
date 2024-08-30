4fdf8527328e453d57152395b6e3504dea2f2dc5
Rastsislau Lipski <rostislav.lipsky@gmail.com>
2024-08-31 00:30:43 +0300

    Configure CORS

---
+++ homebank-api/src/main/kotlin/rastsislaux/homebank/api/application/cors/CorsConfiguration.kt
@@ -0,0 +1,22 @@
+package rastsislaux.homebank.api.application.cors
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer =
+        object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry
+                    .addMapping("/**") // Allow CORS for all endpoints
+                    .allowedOriginPatterns("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+}
